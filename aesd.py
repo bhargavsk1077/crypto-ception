e9afe128519ba26052e3c8aa64bcdec93df9536d
bhargavsk1077 <51043479+bhargavsk1077@users.noreply.github.com>
2020-01-19 17:59:42 +0530

    Update aesd.py

--- aesd.py
+++ aesd.py
@@ -50,33 +50,8 @@ file = open('key1.key', 'rb')
 key1_enc = file.read() # The key will be type bytes
 file.close()
 
-spriv=b'''-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEA5Q5nmx7aHoCMJMsyCxHisKBEH8N27QQE56X2zfNyWDJR4WuO
-wsYkJpP/UkCqcpJCs6f/QS8GmwU8P4peWPZ1HYqYi12LQhPJfWemVLM5JlxuRw5a
-S1upD13i1xyMsosw80iRBaYev5VkModv8flvWVte78NcmtQfiC0bU/BgSgRGegaH
-+PORgeHfLi4vaEmiaMTFpzuy6R+u9dtjGGmgbEZN23bccs0hfqCimbJ8zaWF4cZb
-iZpZJNKvaQ7pLML8qNu5bVV5T+f3YeW/jf92XLgTQbt/fgXXcu0Oyfkt8PWxbd6s
-8jSGZGYVMyRk2t807BMj7nlHE8B7qc2xR+DPmwIDAQABAoIBAA7mQ/kEJb4MMJGI
-IiY0MKG6mxPR3B+IvmTvF8HHzy1LgKYAIBYtW1ajE92e1TeEqhATonfz/iMBUSz0
-7DQvO+kDBe8y04cl0Fp27ovd6J7FLmsy1M7IFJQqUEIb2k8W3MVGeCB88M0Xg6AB
-zVdZGVQfrGo+M/ziRXbPMFLidPD7fLCDlXb3qPnZSunlXzZcdEQwloN+BjrSsgsl
-XyccD3YTPNeRofaku1rVkudw5ddZG0okYU7aPHq0jz4Rx7yNeQgmCNCTILWgEWeK
-npotdvBTaCuadx+c7c1A1MRy6fo4EqMfm/wxwiQDcKEpG7vlfMX9qFv3UYzc8QV4
-oWCocUECgYEA6fxfPfFMqQ5aOFJi2dFB3eZ/LeBblNcfsBAJZnxrPRJDEwCsHhUc
-u+JjNsVP61JeHNQDCs/wzyQZYCBEGx+J9jEvMbUDsPsOZ8BBaOda7grspqYuG9xj
-fRqUIQB9Qm4WguTnultW37SVtSV89YTqRp7vvJ0GtnXBpJ9m1J0fPeECgYEA+ptN
-dAhs120WlsDfvtEACxdExRxqrFd780HavIFgx0Tz3JXS/69/ZRiNELr+vt2liHIh
-B2NlPY+GNXwDzjY5tfYEv4gKmrUBflwpKfwXlZp/EaD6lFQau0Ho/c3TsJe06gEc
-9CmWWYi+3JB2UauEWpsPNIl+uXD8vsFHpOv7pPsCgYEAzWmvOzPg9vEQpy08drhM
-OMAnmmDCRXJt8STC++PySRUFKWOHtokWRqNCMk0aEh6nXGuLmCxg3Zh1ZnwtDhqs
-BSO8qMieyvo79T1ErxGcNCoHA24UQIVEGgoBTM+fJ3h57sOB44pYQ9/HJdYZU3ky
-KnlRQaYgxIGwVBNUNQcS7EECgYAQ+WnynQt6P/pStbex/ggJuEbBLx6Ok9JidKhz
-MjQy6xm1bDSBewqe912+r1vH481tg4V3MaVO2STXBJJhakzYZVSHAJjvR6lVPLrR
-DDEwwekvcX7ngxbYbitw3XskL/JiEzc51oNHhzqeR+6rs3lghbYu8c0ylFOaPA81
-PYKq9wKBgA7mzcORhagZ1DN71jwvI1AgLBIcxWOExmvy/Gv+nYXA9vtFILmtqOt5
-OX3DnUPK2tgbdeFEY4ZaUFnQeHxE0CzUZjKjde790Lj/9rl1WEg4yxxB/mnDvJHI
-UOR/tMJhXmGDdwbMo+Jua5eDbkhnCJd4U5lfyAdWrlAK2ZUejL+F
------END RSA PRIVATE KEY-----'''
+spriv=b'''your hardcoded rsa private key or import .pem file directly in the next line'''
+#keypub = RSA.import_key(open("your-key-name.pem").read())
 keypub = RSA.import_key(spriv)
 cipher_rsa1 = PKCS1_OAEP.new(keypub)
 key1 = cipher_rsa1.decrypt(key1_enc)
